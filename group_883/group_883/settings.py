403fa1e7f53e64f91911bdc456c76800e7ae7722
vitaliiloboda <vitalii.loboda@gmail.com>
2022-05-30 19:27:29 +0300

    delete admin app

--- group_883/group_883/settings.py
+++ group_883/group_883/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import mainapp.apps
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -37,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mainapp.apps.MainappConfig',
+    'personal_account.apps.PersonalAccountConfig',
+    'rating.apps.RatingConfig',
+    'search.apps.SearchConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
