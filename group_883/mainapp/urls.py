f675fb5aa6f7591cad1a9ca6835cfd630b6b5509
vitaliiloboda <vitalii.loboda@gmail.com>
2022-05-31 22:47:04 +0300

    isue_v_1 add main page, categoty page, article page and includes

--- group_883/mainapp/urls.py
+++ group_883/mainapp/urls.py
@@ -4,6 +4,8 @@ from . import views as mainapp
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', mainapp.index, name='index')
+    path('', mainapp.index, name='index'),
+    path('category/<int:pk>/', mainapp.category, name='category'),
+    path('article/<int:pk>/', mainapp.article, name='article'),
 ]
 
--- group_883/mainapp/views.py
+++ group_883/mainapp/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Home'
+    }
+    return render(request, 'mainapp/index.html', context)
+
+
+def category(request, pk):
+    context = {
+        'title': 'category_name'
+    }
+    return render(request, 'mainapp/category.html', context)
+
+
+def article(request, pk):
+    context = {
+        'title': 'article_name'
+    }
+    return render(request, 'mainapp/article.html', context)
+
