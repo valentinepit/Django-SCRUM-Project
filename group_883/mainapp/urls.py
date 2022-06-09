1a51a5228d014089438c40810ca9e45cb460d1ca
trmcreates <admin@payperyou.com>
2022-06-09 15:35:58 +0300

    Search by title

--- group_883/mainapp/urls.py
+++ group_883/mainapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views as mainapp
 
+from .views import SearchResultsView
+
 app_name = 'mainapp'
 
 urlpatterns = [
     path('', mainapp.index, name='index'),
+    path('search/', SearchResultsView.as_view(), name='search_results'),
     path('category/<int:pk>/', mainapp.category, name='category'),
     path('article/<int:pk>/', mainapp.article, name='article'),
 ]
