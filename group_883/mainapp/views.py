008f2061580506148fb59620828cef113c575d2d
Dmitry <pentegov_92@mail.ru>
2022-06-10 20:31:15 +0300

    Authorization and register user

--- group_883/mainapp/views.py
+++ group_883/mainapp/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+from mainapp.models import Article, Category, Tag
+from django.views.generic import ListView
 
 
 def index(request):
+    categories = Category.objects.all()
+    articles = Article.objects.all()
+    read_now = Article.objects.all()
+    news = Article.objects.all()
+    tags = Tag.objects.all()
+    best_of_week = Article.objects.all()
+
     context = {
-        'title': 'Home'
+        'title': 'Home',
+        'categories': categories,
+        'articles': articles,
+        'read_now': read_now,
+        'news': news,
+        'tags': tags[:10],
+        'best_of_week': best_of_week
     }
     return render(request, 'mainapp/index.html', context)
 
@@ -20,4 +35,14 @@ def article(request, pk):
         'title': 'article_name'
     }
     return render(request, 'mainapp/article.html', context)
+
+
+class SearchResultsView(ListView):
+    model = Article
+    template_name = 'search.html'
+
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Article.objects.filter(title__icontains=query)
 
