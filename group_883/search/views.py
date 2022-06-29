cebc70031a6f4651e623065c26fa7c0a51150e7f
trmcreates <admin@payperyou.com>
2022-06-29 13:01:32 +0500

    pagination on search complete

--- group_883/search/views.py
+++ group_883/search/views.py
@@ -2,6 +2,7 @@ from .filters import ArticleFilter
 
 from django.views.generic import ListView
 from django.db.models import Count
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from mainapp.models import Article
 
@@ -18,6 +19,7 @@ class SearchResultsView(ListView):
             query = ""
         articles = Article.objects.filter(title__icontains=query)
         my_filter = ArticleFilter(self.request.GET, queryset=articles)
+
         context.update({
             'search_data': query,
             'count': len(Article.objects.filter(title__icontains=query)),
@@ -25,6 +27,21 @@ class SearchResultsView(ListView):
             'articles': my_filter.qs,
         })
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return ArticleFilter(self.request.GET, queryset=queryset).qs
+
+# @api_view(['GET', ])
+# def my_function_based_list_view(request):
+#     paginator = PageNumberPagination()
+#     filtered_set = filters.MyModelFilter(
+#         request.GET,
+#         queryset=MyModel.objects.all()
+#     ).qs
+#     context = paginator.paginate_queryset(filtered_set, request)
+#     serializer = MyModelSerializer(context, many=True)
+#     return paginator.get_paginated_response(serializer.data)
 
 
 class PopularListView(SearchResultsView):
