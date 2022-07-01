7dd1e7e773ec31ed871c9f18f51afd34dd71551f
Shrimp-pixel <aleksander.17047665@gmail.com>
2022-07-01 15:30:53 +0300

    Уведомления работают, но некрасиво. get_article и get_comment в personal_account\models.py импортируют article и comment в самих функциях. Если импортировать на уровне models.py получается ошибка. Как исправить не знаю.

---
+++ group_883/personal_account/signals.py
@@ -0,0 +1,45 @@
+from .models import Notification
+from mainapp.models import Comment, Article
+from personal_account.models import User
+
+from django.db.models.signals import post_save, pre_save, m2m_changed
+from django.dispatch import receiver
+from django.db.models import Q
+
+
+@receiver(post_save, sender=Comment)
+def create_comment(sender, instance, created, **kwargs):
+    if created:
+        if instance.body.startswith('@moderator'):
+            moderators = User.objects.filter(Q(groups__name='admins') | Q(groups__name='moderators'))
+
+            for moderator in moderators:
+                Notification.objects.get_or_create(notification_type=0, to_user=moderator, from_user=instance.user,
+                                                   object_id=instance.article.id, user_has_seen=False)
+        else:
+            if instance.is_parent and instance.article.user != instance.user:
+                Notification.objects.get_or_create(notification_type=2, to_user=instance.article.user,
+                                                   from_user=instance.user, object_id=instance.id, user_has_seen=False)
+            elif instance.parent.user!=instance.user:
+                Notification.objects.get_or_create(notification_type=4, to_user=instance.parent.user,
+                                                   from_user=instance.user, object_id=instance.id, user_has_seen=False)
+
+
+@receiver(m2m_changed, sender=Comment.likes.through)
+def update_comment(sender, instance, action, pk_set=None, **kwargs):
+    if action == 'pre_add':
+        for pk in pk_set:
+            u = User.objects.get(id=pk)
+            if u != instance.user:
+                Notification.objects.get_or_create(notification_type=3, to_user=instance.user,
+                                                   from_user=u, object_id=instance.id, user_has_seen=False)
+
+
+@receiver(m2m_changed, sender=Article.likes.through)
+def update_article(sender, instance, action, pk_set=None, **kwargs):
+    if action == 'pre_add':
+        for pk in pk_set:
+            u = User.objects.get(id=pk)
+            if u != instance.user:
+                Notification.objects.get_or_create(notification_type=1, to_user=instance.user,
+                                                   from_user=u, object_id=instance.id, user_has_seen=False)
