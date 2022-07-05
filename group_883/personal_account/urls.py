552f0d34787ef4487e4006aacb5e3697d0c1ca89
trmcreates <admin@payperyou.com>
2022-07-05 19:19:21 +0500

    Changes: buttons in LK buttons in article delete popular tags on all pages margin-top for button in change password popular tags on change password Button and links on registration form Button and links on login form Category list buttons Moderation list filter for active articles Footer links Category all - add filter is_active Our user buttons and data Mainapp last news filter

--- group_883/personal_account/urls.py
+++ group_883/personal_account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from personal_account.views import register, edit, login, logout, CreateArticle, EditArticle, ListArticle, \
     DeleteArticle, password_change_done, UserDetail, user, verify, create_permissions, delete_permissions, delete_user,\
-    PostNotification, FollowNotification, RemoveNotification
+    PostNotification, FollowNotification, RemoveNotification, PasswordChange
 
 app_name = 'personal_account'
 
@@ -19,7 +19,7 @@ urlpatterns = [
     path('list_article/delete_article/<int:pk>/', login_required(DeleteArticle.as_view()), name='delete_article'),
     path('list_article/create_article/', login_required(CreateArticle.as_view()), name='create_article'),
     path('pasword_change/',
-         PasswordChangeView.as_view(template_name='personal_account/password_change.html',
+         PasswordChange.as_view(template_name='personal_account/password_change.html',
                                     success_url=reverse_lazy('personal_account:password_change_done')),
          name='password_change'),
     path('password_change/done/', password_change_done, name='password_change_done'),
